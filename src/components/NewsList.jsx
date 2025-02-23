b7a14a4e691c00483bff21d926e51e89cbb92ce3
RyuuG <140732721+Ryuu-G@users.noreply.github.com>
2025-02-23 19:55:33 +0700

    Feat: News list

---
+++ src/components/NewsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NewsList = ({ berita }) => {
+  if (!Array.isArray(berita) || berita.length === 0) {
+    return <p>No news found.</p>; // Menangani kasus tidak ada berita
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {berita.map((item) => (
+        <div key={item.berita_id} className="bg-gray-800 p-4 rounded-2xl shadow-md">
+          <img src={"https://jkt48.com" + item.badge_url} alt="Badge" className="news-badge mb-2" />
+          <h3 className="text-lg font-bold">{item.judul}</h3>
+          <p className="text-sm text-gray-400">{item.waktu}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NewsList;

7c3e40335dd9712f579c45cf1518904f16e778dd
Ryuu <auryfieroumarva@gmail.com>
2025-02-24 14:23:58 +0700

    Fix: Update News & Schedule

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -9,7 +9,7 @@ const NewsList = ({ berita }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {berita.map((item) => (
         <div key={item.berita_id} className="bg-gray-800 p-4 rounded-2xl shadow-md">
-          <img src={"https://jkt48.com" + item.badge_url} alt="Badge" className="news-badge mb-2" />
+          <img src={"https://jkt48.com" + item.badge_url} alt="Badge" className="news-badge mb-2 rounded-md" />
           <h3 className="text-lg font-bold">{item.judul}</h3>
           <p className="text-sm text-gray-400">{item.waktu}</p>
         </div>
--- src/components/ScheduleList.jsx
+++ src/components/ScheduleList.jsx
@@ -9,8 +9,8 @@ const ScheduleList = ({schedule}) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {schedule.map((item, i) => (
         <div key={i} className="bg-gray-800 p-4 rounded-2xl shadow-md">
-          <h3 className="text-lg font-bold">{item.showInfo}</h3>
-          <p className="text-sm text-gray-400">Setlist: {item.setlist}</p>
+          <h3 className="text-lg font-bold">{item.setlist}</h3>
+          <p className="text-sm text-gray-400">Date: {item.showInfo.split('Show')[0]}</p>
           <p className="text-sm text-gray-400">Time: {item.showInfo.split(' ')[2]}</p>
           <p className="text-sm text-gray-400">Members:</p>
           <ul className="list-disc list-inside">
